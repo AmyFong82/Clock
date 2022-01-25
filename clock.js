681eea0b5710e3cb8f033d6e9b6802892cf2bbef
AmyFong82 <amyhfong@gmail.com>
2022-01-25 12:50:15 -0500

    added minute hand js.

--- clock.js
+++ clock.js
@@ -2,10 +2,14 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
   function run(){
     const sec_hand = document.querySelector('#second-hand')
+    const min_hand = document.querySelector('#minute-hand')
     let now = new Date()
     let second = now.getSeconds()
-    let deg = second * 6
-    sec_hand.style.transform = "rotate(" + deg + "deg)"
+    let minute = now.getMinutes()
+    let sec_deg = second * 6
+    let min_deg = minute * 6
+    sec_hand.style.transform = "rotate(" + sec_deg + "deg)"
+    min_hand.style.transform = "rotate(" + min_deg + "deg)"
   }
 
   
